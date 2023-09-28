d201374e01a788390799d5cab6f06bbd7bfe1db9
karianov.d.n <karianov.d.n@gmail.com>
2023-09-28 23:04:47 +0200

    redirect, templates, tegs

--- women/views.py
+++ women/views.py
@@ -1,8 +1,30 @@
 from django.http import HttpResponse, HttpResponseNotFound
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.template.loader import render_to_string
+from django.urls import reverse
+
+
+menu = ['О сайте', 'Добавить статью', 'Обратная связь', 'Войти']
+
+
+data_db = [
+    {'id': 1, 'title': 'Анджелина Джоли', 'content': 'Биография Анджелины Джоли', 'is_published': True},
+    {'id': 2, 'title': 'Марго Робби', 'content': 'Биография Марго Робби', 'is_published': False},
+    {'id': 3, 'title': 'Джулия Робертс', 'content': 'Биография Джулия Робертс', 'is_published': True},
+]
+
 
 def index(request):
-    return HttpResponse('Страница приложения women.')
+    data = {
+        'title': 'Главная страница',
+        'menu': menu,
+        'posts': data_db,
+    }
+    return render(request, 'women/index.html', context=data)
+
+
+def about(request):
+    return render(request, 'women/about.html', {'title': 'О сайте'})
 
 
 def categories(request, cat_id):
@@ -15,6 +37,10 @@ def categories_by_slug(request, cat_slug):
 
 
 def archive(request, year):
+    if year > 2023:
+        uri = reverse('cats', args=('music', ))
+        return redirect(uri)
+
     return HttpResponse(f'<h1>Архив по годам</h1><p>{year}</p>')
 
 
