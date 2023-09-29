622d0312541dca601e746879b367cfacd22eb7ee
karianov.d.n <karianov.d.n@gmail.com>
2023-09-29 17:04:49 +0200

    extend and include templates, connect static

--- sitewomen/settings.py
+++ sitewomen/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_c9t*g@=@)vl!ed@mbu1_q4u5yfr=$we6c4aa42xvv_9!nku%7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'sitewomen.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
