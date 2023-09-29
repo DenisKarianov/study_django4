6c79c3f130497f6e822737ff57ae7fa5fb589f0a
karianov.d.n <karianov.d.n@gmail.com>
2023-09-29 23:17:46 +0200

    user template tegs

--- women/views.py
+++ women/views.py
@@ -26,11 +26,19 @@ data_db = [
 ]
 
 
+cats_db = [
+    {'id': 1, 'name': 'Актрисы'},
+    {'id': 2, 'name': 'Певицы'},
+    {'id': 3, 'name': 'Спортсменки'},
+]
+
+
 def index(request):
     data = {
         'title': 'Главная страница',
         'menu': menu,
         'posts': data_db,
+        'cat_selected': 0,
     }
     return render(request, 'women/index.html', context=data)
 
@@ -53,6 +61,16 @@ def contact(request):
 
 def login(request):
     return HttpResponse("Авторизация")
+
+
+def show_catogory(request, cat_id):
+    data = {
+        'title': 'Отображение по рубрикам',
+        'menu': menu,
+        'posts': data_db,
+        'cat_selected': cat_id,
+    }
+    return render(request, 'women/index.html', context=data)
 
 
 def page_not_found(request, exception):
